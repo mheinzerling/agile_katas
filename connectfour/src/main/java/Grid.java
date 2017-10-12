0511ca9878920960a867221d7c04fd2504011915
Martin Heinzerling <webmaster@mheinzerling.de>
2017-10-12 11:04:16 +0200

    integrate column into grid

--- connectfour/src/main/java/Grid.java
+++ connectfour/src/main/java/Grid.java
@@ -2,13 +2,21 @@ import java.util.Arrays;
 
 class Grid
 {
-	private final Column[] columns;
+	private final Token[][] grid;
+	private final int[]     nextTokenHeights;
 
 	public Grid(int numberOfRows, int numberOfColumns)
 	{
-		columns = new Column[numberOfColumns];
-		for( int i = 0; i < numberOfColumns; i++ )
-			columns[i] = new Column(numberOfRows);
+		grid = new Token[numberOfColumns][numberOfRows];
+		nextTokenHeights = new int[numberOfColumns];
+		for( int c = 0; c < numberOfColumns; c++ )
+		{
+			for( int r = 0; r < numberOfRows; r++ )
+			{
+				grid[c][r] = Token.NONE;
+			}
+			nextTokenHeights[c] = 0;
+		}
 	}
 
 	public Token[] getRightDiagonalOfTokensAlong(int columnWhereTokenIsPlaced, int rowWhereTokenIsPlaced)
@@ -16,9 +24,9 @@ class Grid
 		Token[] tokens = new Token[getNumberOfColumns()];
 		for( int rowNumber = rowWhereTokenIsPlaced, columnNumber = columnWhereTokenIsPlaced;
 			 rowNumber >= 0 && columnNumber < getNumberOfColumns(); rowNumber--, columnNumber++ )
-			tokens[columnNumber] = getColorOfTokenPlacedIn(rowNumber, columnNumber);
+			tokens[columnNumber] = grid[columnNumber][rowNumber];
 		for( int rowNumber = rowWhereTokenIsPlaced, columnNumber = columnWhereTokenIsPlaced; rowNumber < getNumberOfRows() && columnNumber >= 0; rowNumber++, columnNumber-- )
-			tokens[columnNumber] = getColorOfTokenPlacedIn(rowNumber, columnNumber);
+			tokens[columnNumber] = grid[columnNumber][rowNumber];
 		return tokens;
 	}
 
@@ -26,22 +34,25 @@ class Grid
 	{
 		Token[] tokens = new Token[getNumberOfColumns()];
 		for( int rowNumber = rowWhereTokenIsPlaced, columnNumber = columnWhereTokenIsPlaced; rowNumber >= 0 && columnNumber >= 0; rowNumber--, columnNumber-- )
-			tokens[columnNumber] = getColorOfTokenPlacedIn(rowNumber, columnNumber);
+			tokens[columnNumber] = grid[columnNumber][rowNumber];
 		for( int rowNumber = rowWhereTokenIsPlaced, columnNumber = columnWhereTokenIsPlaced;
 			 rowNumber < getNumberOfRows() && columnNumber < getNumberOfColumns(); rowNumber++, columnNumber++ )
-			tokens[columnNumber] = getColorOfTokenPlacedIn(rowNumber, columnNumber);
+			tokens[columnNumber] = grid[columnNumber][rowNumber];
 		return tokens;
 	}
 
 	public int put(Token token, int column) throws FullColumnException, NonexistentColumnException
 	{
 		checkIfColumnExists(column);
-		return columns[column].place(token);
+		if( nextTokenHeights[column] >= getNumberOfRows() ) throw new FullColumnException("Column is full!");
+		grid[column][nextTokenHeights[column]] = token;
+		nextTokenHeights[column]++;
+		return nextTokenHeights[column] - 1;
 	}
 
 	private void checkIfColumnExists(int columnNumber) throws NonexistentColumnException
 	{
-		if( columnNumber < 0 || columnNumber >= columns.length ) throw new NonexistentColumnException("Column does not exist");
+		if( columnNumber < 0 || columnNumber >= grid.length ) throw new NonexistentColumnException("Column does not exist");
 	}
 
 	public Token[] getColumnOfTokensAlong(int columnNumber)
@@ -49,14 +60,9 @@ class Grid
 		Token[] tokens = new Token[getNumberOfColumns()];
 		for( int rowNumber = 0; rowNumber < getNumberOfRows(); rowNumber++ )
 		{
-			tokens[rowNumber] = getColorOfTokenPlacedIn(rowNumber, columnNumber);
+			tokens[rowNumber] = grid[columnNumber][rowNumber];
 		}
 		return tokens;
-	}
-
-	private Token getColorOfTokenPlacedIn(int rowWhereTokenIsPlaced, int columnNumber)
-	{
-		return columns[columnNumber].getToken(rowWhereTokenIsPlaced);
 	}
 
 	public Token[] getRowOfTokensAlong(int rowWhereTokenIsPlaced)
@@ -64,7 +70,7 @@ class Grid
 		Token[] tokens = new Token[getNumberOfColumns()];
 		for( int columnNumber = 0; columnNumber < getNumberOfColumns(); columnNumber++ )
 		{
-			tokens[columnNumber] = getColorOfTokenPlacedIn(rowWhereTokenIsPlaced, columnNumber);
+			tokens[columnNumber] = grid[columnNumber][rowWhereTokenIsPlaced];
 		}
 		return tokens;
 	}
@@ -72,17 +78,17 @@ class Grid
 	public int getNumberOfTokensIn(int columnNumber) throws NonexistentColumnException
 	{
 		checkIfColumnExists(columnNumber);
-		return columns[columnNumber].getCurrentNumberOfTokens();
+		return nextTokenHeights[columnNumber];
 	}
 
 	public int getNumberOfRows()
 	{
-		return columns[0].getNumberOfRows();
+		return grid[0].length;
 	}
 
 	public int getNumberOfColumns()
 	{
-		return columns.length;
+		return grid.length;
 	}
 
 	@Override
